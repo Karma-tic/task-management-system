9afecd56d33dab360a36d8e5045710f3096effac
Sujeet Singh <singhsujeet9754@gmail.com>
2025-12-29 19:01:50 +0530

    Add MongoDB connection using Mongoose

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 dotenv.config();
 
+// Connect database
+connectDB();
+
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
-// route
 app.get("/", (req, res) => {
   res.status(200).send("Task Manager API is running successfully");
 });
@@ -19,7 +21,7 @@ app.get("/health", (req, res) => {
   res.status(200).json({ status: "OK" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
